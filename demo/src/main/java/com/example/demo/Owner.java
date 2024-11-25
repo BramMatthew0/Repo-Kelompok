94c064b23962856fd3b6bf8b92da75f2e797c1d9
Audric Aurelius <6182101039@student.unpar.ac.id>
2024-11-26 01:09:41 +0700

    first try

--- demo/src/main/java/com/example/demo/Owner.java
+++ demo/src/main/java/com/example/demo/Owner.java
@@ -1,5 +1,11 @@
 package com.example.demo;
+import lombok.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Owner {
-    
+    private String username;   // Nama pegawai
+    private String password; // Password pegawai
 }
